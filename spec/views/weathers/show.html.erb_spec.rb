2909d22bab43475015ce9d286d6aed95be225112
Eric Wee <wee007@gmail.com>
2018-05-13 00:17:31 +1000

    Create weather page

--- spec/views/weathers/show.html.erb_spec.rb
+++ spec/views/weathers/show.html.erb_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "weathers/show.html.erb", type: :view do
   describe "rendering the show template" do
-    let(:data) do
+    let(:data) {
       {
         title: "Yahoo! Weather - Melbourne, VIC, AU",
         last_build_date: "Wed, 09 May 2018 04:41 PM AEST"
       }
-    end
+    }
 
     it "displays the show page" do
       assign(:weather, data)
 
       render
 
-      expect(rendered).to match /Yahoo/
+      expect(rendered).to match /Forecast/
     end
   end
 end
